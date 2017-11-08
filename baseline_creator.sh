3d073ae1800fd61d5203d17cbb0b6df3f45025af
marisafromthere <matki002@plattsburgh.edu>
2017-11-07 23:03:28 -0500

    REALLY polished b_c -still not comments

--- baseline_creator.sh
+++ baseline_creator.sh
@@ -3,18 +3,23 @@
 #This will loop through the files in a directory (Put in loop of directories)
 
 #://askubuntu.com/questions/678914/loop-through-all-files-in-a-folder
-function makeBase() {
+function createBaseline() {
     ABSPATH=$(find / -name Baselines 2>/dev/null)
-    prefix=$(echo -n $1 | rev | cut -d "/" -f 1 | rev)
-    suffix="_baseline.txt"
-    filename="$prefix$suffix"
-    checker=$(nameCheck $filename $ABSPATH | head -n 1)
+    file=$(makeBase $1)
+    checker=$(nameCheck $file $ABSPATH | head -n 1)
     if [ $checker -ne 1 ]; then
         baselineWrite $1
-        mv temp.txt "$ABSPATH/$(echo $filename)"
+        mv temp.txt "$ABSPATH/$(echo $file)"
     else
         echo "Oops, you already took this baseline"
     fi
+}
+
+function makeBase() {
+    prefix=$(echo -n $1 | rev | cut -d "/" -f 1 | rev)
+    suffix="_baseline.txt"
+    filename="$prefix$suffix"
+    echo $filename
 }
 
 function baselineWrite() { 
@@ -39,4 +44,4 @@ function nameCheck(){
     done
     echo 0
 }
-makeBase "$PWD" "File.txt"
+createBaseline "$PWD" "File.txt"
