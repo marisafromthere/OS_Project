ce1fe4b0d27e6874d047e6f4ed95dff29d7e587a
marisafromthere <matki002@plattsburgh.edu>
2017-11-07 22:20:38 -0500

    Finished baseline_creator.sh

--- baseline_creator.sh
+++ baseline_creator.sh
@@ -1,22 +1,41 @@
-#! /bin/sh
+#!/bin/bash
 
 #This will loop through the files in a directory (Put in loop of directories)
 
 #://askubuntu.com/questions/678914/loop-through-all-files-in-a-folder
+function makeBase() {
+    prefix=$(echo -n $1 | rev | cut -d "/" -f 1 | rev)
+    suffix="_baseline.txt"
+    filename="$prefix$suffix"
+    checker=$(nameCheck $filename | head -n 1)
+    if [ $checker -ne 1 ]; then
+        baselineWrite $1
+        mv temp.txt "$PWD/Baselines/$(echo $filename)"
+    else
+        echo "Oops, you already took this baseline"
+    fi
+}
 
-function loop() { 
+function baselineWrite() { 
     for i in "$1"/*
     do
         if [ -d "$i" ]; then
-            loop "$i"
+            ls -l -d $i
+	    baselineWrite "$i"
         elif [ -e "$i" ]; then
-            echo $i
-        else
-            echo "$i"" - Folder Empty"
+            ls -l $i
         fi
-    done
+    done >> temp.txt
 }
 
-loop "$PWD"
-
-
+function nameCheck(){
+    for i in "$PWD/Baselines"/*
+    do
+        if [ "$(echo -n $i | rev | cut -d "/" -f 1 | rev)" == "$1" ]; then
+            echo 1
+            break
+        fi
+    done
+    echo 0
+}
+makeBase "$PWD" "File.txt"
