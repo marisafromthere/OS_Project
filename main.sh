2a0621eb3a710457752aebf48516f3dc386e8dca
marisafromthere <matki002@plattsburgh.edu>
2017-11-11 12:44:43 -0500

    finished main file minus reports bc it doesn't exist yet

--- main.sh
+++ main.sh
@@ -5,17 +5,16 @@ main() {
     echo "What are you looking to do today? Create Baseline(1) Run Check(2) Generate report(3)"
 
     read opt
-    while [ "$opt" != "q" ] || [ "$opt" != "quit" ] || [ "opt" !=  "exit" ]
+    while [ "$opt" != "q" ]
     do
-        case $opt in 
-        if [ "$opt" == "1" ];
-        then
+        case $opt in
+        [1]*)
             echo "Please enter a Directory to create a baseline of: "
             read dir
             source ./baseline_creator.sh "$dir"
+            ;;
 
-        elif [ "$opt" == "2" ];
-        then
+        [2]*)
             echo "WARNING! Running a check destroys any data stored in the temp_Reports file. Do you wish to continue? (y/n) "
             read ans
             if [ "$ans" == "y" ]; then
@@ -23,12 +22,13 @@ main() {
                 read dir
                 source ./run_check.sh "$dir"
             fi
+            ;;
 
-        elif [ "$opt" == "3" ];
-        then
-            echo "Reporting is in a trial phase, but here's what you can do!"
-
-        fi
+        [3]*)
+            echo "Reporting is not complete yet, but here's your temp_Report"
+            cat ./Reports/temp_Report.txt
+            ;;
+        esac
 
     echo "Would you like to do anything else? Create Baseline(1) Run Check(2) Generate report(3) Quit(q) "
     read opt
